38c8d32b910824dbc329ecf2de11b886daf21cf2
Nikos Andriopoulos <nickos2014andriopoulos@gmail.com>
2023-03-22 19:27:34 +0200

    Fix Css errors

--- src/Components/FinalScore.jsx
+++ src/Components/FinalScore.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useQuizContext } from "../QuizProvider";
+import styles from "./styles/final-score.module.css";
+
 // ! Do Not Add Props Here
 export const FinalScore = () => {
   const { correct, inCorrect } = useQuizContext();
   return (
-    <div id="final-score">
+    <div className={`${styles["final-score"]}`}>
       <h1>Your Final Score Was</h1>
-      <div id="score">
+      <div className={`${styles["final-score"]}`}>
         <p>{correct}</p>
         <hr />
         <p>{correct + inCorrect}</p>
